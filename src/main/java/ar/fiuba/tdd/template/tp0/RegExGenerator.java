b75c2eaedb3c6050f399394385327f051615fb34
Key Nakasone <Key Nakasone>
2016-03-14 15:06:59 -0300

    Descomentando poco a poco, pero dejandolo en un estado sin warning ni errores, para luego proceder al paso de implementacion

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RegExGenerator {
-    // TODO: Uncomment this field
-    //private int maxLength;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    private int maxLength;
 
-    // TODO: Uncomment parameters
-    public List<String> generate(/*String regEx, int numberOfResults*/) {
+    public RegExGenerator(int maxLength) {
+       this.maxLength = maxLength;
+    }
+
+
+    public List<String> generate(String regEx, int numberOfResults) {
+
         return new ArrayList<String>() {
             {
                 add("a");
--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -11,9 +11,10 @@ import static org.junit.Assert.assertTrue;
 public class RegExGeneratorTest {
 
     private boolean validate(String regEx, int numberOfResults) {
-        RegExGenerator generator = new RegExGenerator();
+        int maxLength = 10;
+        RegExGenerator generator = new RegExGenerator(maxLength);
         // TODO: Uncomment parameters
-        List<String> results = generator.generate(/*regEx, numberOfResults*/);
+        List<String> results = generator.generate(regEx, numberOfResults);
         // force matching the beginning and the end of the strings
         Pattern pattern = Pattern.compile("^" + regEx + "$");
         return true;
