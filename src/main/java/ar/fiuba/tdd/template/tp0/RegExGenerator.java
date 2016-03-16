8ee16a40fb50f8515ba06cd5737a86767254ae5a
Key Nakasone <Key Nakasone>
2016-03-15 22:12:52 -0300

    testAnyCharacter passed

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -1,11 +1,12 @@
 package ar.fiuba.tdd.template.tp0;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class RegExGenerator {
 
     private int maxLength = 10;
+    private static int DOT = '.';
+
 
     public RegExGenerator(int maxLength) {
         this.maxLength = maxLength;
@@ -13,17 +14,31 @@ public class RegExGenerator {
 
     public List<String> generate(String regEx, int numberOfResults) {
 
+        Random randomNumber = new Random();
+        ArrayList<String> matchedStrings = new ArrayList<String>();
+
         if (maxLength == 10) {
-            System.out.println("Hola mundo");
+            System.out.println("todavia no lo use");
         }
 
+        String oneMatch = "";
+        for (int i = 0 ; i < regEx.length() ; i++) {
+            if (regEx.charAt(i) == DOT) {
+                int oneRandom = randomNumber.nextInt(256);
+                oneMatch = oneMatch.concat(String.valueOf(Character.toChars(oneRandom)));
+            }
+        }
+        matchedStrings.add(oneMatch);
 
-        return new ArrayList<String>() {
-            {
+        return matchedStrings;
+    }
+
+/*        return new ArrayList<String>() {
+           {
                 add("a");
                 add("b");
                 add("c");
             }
         };
-    }
+    }*/
 }
--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -28,12 +28,12 @@ public class RegExGeneratorTest {
     }
 
     //TODO: Uncomment these tests
-    /*
+
     @Test
     public void testAnyCharacter() {
         assertTrue(validate(".", 1));
     }
-
+    /*
     @Test
     public void testMultipleCharacters() {
         assertTrue(validate("...", 1));
